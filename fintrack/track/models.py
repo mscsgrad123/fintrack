619a68c5b189624185496133244bdd73cc343dc7
Mahima Mannava <mahimamannava@Mahimas-MBP.attlocal.net>
2024-11-30 21:08:48 -0800

    Views are updated

--- fintrack/track/models.py
+++ fintrack/track/models.py
@@ -8,21 +8,28 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
 
-class Category(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-
 class TransactionType(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description=models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    #transaction_type = models.ForeignKey(TransactionType, default = ('expense', 1), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 class Transaction(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateField()
-    type = models.ForeignKey(TransactionType,on_delete=models.CASCADE)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    transaction_type_id = models.ForeignKey(TransactionType,on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category,on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=100)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)    
--- fintrack/track/views.py
+++ fintrack/track/views.py
@@ -1,5 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+from .models import *
+#from .forms import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+import json, logging
+from django.utils import timezone
 
+
+logger = logging.getLogger(__name__)
 # Create your views here.
+def create_transaction(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            user = get_object_or_404(request.user)
+            transaction_type = get_object_or_404(TransactionType, id=data['id'])
+            category = get_object_or_404(Category, id=data['category_id'])
 
+            transaction = Transaction.objects.create(
+                date = timezone.now(),
+                type = transaction_type,
+                category = category,
+                amount = data['amount'],
+                description = data['description'],
+                user_id = user
+            )
+            logger.error(f"Error occurred: {str(e)}")
+            response_data = {'message': 'Transaction created successfully', 'id': transaction.id}
+            return Response(json.dumps(response_data), content_type="application/json", status=201)
+        
+        except Exception as e:
+            response_data = {'message': str(e)}
+            return Response(json.dumps(response_data), content_type="application/json", status=400)
+        return Response(json.dumps({'error':'Invalid Http Method'}), content_type="application/json", status=405)
+
+def update_transaction(request, transaction_id):
+    if request.method == "PUT":
+        try:
+            data = json.loads(request.body) 
+            transaction = get_object_or_404(Transaction, id=id)
+            transaction.date = data.get('date', transaction.date)
+            transaction.type = get_object_or_404(TransactionType, id=data.get['type_id']) if 'type_id' in data else transaction.type
+            transaction.category = get_object_or_404(Category, id=data.get ['category_id']) if 'category_id' in data else transaction.category
+            transaction.amount = data.get('amount', transaction.amount)
+            transaction.description = data.get('description', transaction.description)
+            transaction.save()
+            response_data = {'message': 'Transaction updated successfully'}
+            return Response(json.dumps(response_data), content_type="application/json", status=200)
+        except Exception as e:
+            response_data = {'message': str(e)}
+            return Response(json.dumps(response_data), content_type="application/json", status=400)
+        return Response(json.dumps({'error':'Invalid Http Method'}), content_type="application/json", status=405)
+
+
+def delete_transaction(request, transaction_id):
+    if request.method == 'DELETE':
+        try:
+            transaction = get_object_or_404(Transaction, id=transaction_id)
+            transaction.delete
+            response_data = {'message': 'Transaction deleted successfully'}
+            return Response(json.dumps(response_data), content_type="application/json", status=200)
+        except Exception as e:
+            response_data = {'message': str(e)}
+            return Response(json.dumps(response_data), content_type="application/json", status=400)
+        return Response(json.dumps({'error':'Invalid Http Method'}), content_type="application/json", status=405)
 
